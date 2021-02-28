ca5dcb516caf145200b600b7047012fbfb42d30c
MD Sofie Uddin Sohel <engrsofie@gmail.com>
2021-02-28 22:27:53 +0600

    added a header

---
+++ amazon/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header/Header';
+import Product from './components/Product/Product';
+import Shop from './components/Shop/Shop';
+
+
+
+function App() {
+  return (
+    <div>
+     <Header></Header>
+     <Shop></Shop>
+    </div>
+  );
+}
+
+export default App;
